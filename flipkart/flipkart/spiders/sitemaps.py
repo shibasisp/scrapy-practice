80d9f6b67da5929933fd155c6a7131f768b8b4c1
Shibasis Patel <patelshibasis@gmail.com>
2018-01-13 12:11:33 +0530

    Now I can populate all the populate all the links from sitemaps from robot.txt itself

--- flipkart/flipkart/spiders/sitemaps.py
+++ flipkart/flipkart/spiders/sitemaps.py
@@ -2,18 +2,18 @@ from scrapy.spiders import SitemapSpider
 
 class MySpider(SitemapSpider):
     name = 'sitemaps'
-    sitemap_urls = ['http://www.flipkart.com/sitemap_o_index.xml',
-                    'http://www.flipkart.com/sitemap_q_index.xml',
-                    'http://www.flipkart.com/sitemap_d_index.xml']
-    # sitemap_rules = ['/q/','parse_sitemap_url']
+    sitemap_urls = ['https://www.flipkart.com/robots.txt']
+    rules = [ 
+      ("/sitemap*", "parse_sitemap_url"), 
+   ]
+    sitemap_rules = ['/q/','parse_q']
     def parse(self, response):
-        yield {
-            'title': response.css("title ::text").extract_first(),
-            'url': response.url
-        }
+        pass
 
     def parse_sitemap_url(self, response):
         yield {
             'title': response.css("title ::text").extract_first(),
             'url': response.url
         }
+    def parse_q(self, response):
+        pass
